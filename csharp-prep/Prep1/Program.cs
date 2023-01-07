051d909eecabb6b16240f360eb6833726d7c0b8c
00codester <00codester@gmail.com>
2023-01-07 16:36:27 -0700

    added comments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Asks user for first name
         Console.Write("What is your first name? ");
         string firstname = Console.ReadLine();
-
+        //Asks user for last name
         Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();
-
+        //prints the james bond style name phrase
         Console.WriteLine($"\nYour name is {lastname}, {firstname} {lastname}.");
 
     }
