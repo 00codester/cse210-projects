2433c2e41146830c320ab330ecaf47747ca4bf4f
00codester <00codester@gmail.com>
2023-02-15 22:10:50 -0700

    Completed Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mass = new MathAssignment("Roberto Rodriquez", "Fractions", "7.3", "8-19");
+        Console.WriteLine();
+        Console.WriteLine(mass.GetSummary());
+        Console.WriteLine(mass.GetHomeworkList());
+
+        WritingAssignment wass = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine();
+        Console.WriteLine(wass.GetSummary());
+        Console.WriteLine(wass.GetWritingInformation());
     }
 }
